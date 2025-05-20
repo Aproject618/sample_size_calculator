8355f8a93c2ed9c3ac3be341b08263e486537b8e
Aproject618 <56072140+Aproject618@users.noreply.github.com>
2025-05-20 10:17:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sample-size-calculator',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python package for calculating required sample sizes for A/B testing using z-tests',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
